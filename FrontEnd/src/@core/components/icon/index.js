4cd1e0cbf74468db13f5be779c7341eb797e8bb8
Mohammad-Abohasan <m.s.abuhasan@students.ptuk.edu.ps>
2023-04-29 17:33:42 +0300

    delete template

--- FrontEnd/src/@core/components/icon/index.js
+++
@@ -1,8 +0,0 @@
-// ** Icon Imports
-import { Icon } from '@iconify/react'
-
-const IconifyIcon = ({ icon, ...rest }) => {
-  return <Icon icon={icon} fontSize='1.375rem' {...rest} />
-}
-
-export default IconifyIcon
