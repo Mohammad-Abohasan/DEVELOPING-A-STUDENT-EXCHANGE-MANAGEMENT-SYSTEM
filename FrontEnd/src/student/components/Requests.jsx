26b777b2add834b28b258de668062aa41642199a
Mohammad-Abohasan <m.s.abuhasan@students.ptuk.edu.ps>
2023-05-05 07:17:31 +0300

    Available Offer V1

--- FrontEnd/src/student/components/Requests.jsx
+++ FrontEnd/src/student/components/Requests.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import Sidebar from "./Sidebar"
+import Navbar from "./Navbar"
 
 const Requests = () => {
   return (
-    <div>Requests</div>
+    <>
+      <Navbar />
+      <div className="container-fluid py-3 pt-4">
+        <div className="row">
+          <div className="col-2 sidebar">
+            <Sidebar />
+          </div>
+          <div className="col-10">
+            <div>Requests</div>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
